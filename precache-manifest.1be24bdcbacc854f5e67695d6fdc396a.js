997b6c7812551463b41a87346bedc7afd302e716
giraffes101 <bbkessler@icloud.com>
2020-05-18 18:47:11 -0500

    Updates

--- precache-manifest.1be24bdcbacc854f5e67695d6fdc396a.js
+++ precache-manifest.1be24bdcbacc854f5e67695d6fdc396a.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dbfe436af7a851f52d8a09d4dd96ebdd",
+    "revision": "367a622e6d5cae5df21880d8f416ec95",
     "url": "/index.html"
   },
   {
-    "revision": "625c74636045738258a7",
+    "revision": "df103897fab05a81360a",
     "url": "/static/css/main.5f361e03.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.b03b2dac.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "625c74636045738258a7",
-    "url": "/static/js/main.33cfe52e.chunk.js"
+    "revision": "df103897fab05a81360a",
+    "url": "/static/js/main.36fed01d.chunk.js"
   },
   {
     "revision": "10812e2704e22d7b58eb",

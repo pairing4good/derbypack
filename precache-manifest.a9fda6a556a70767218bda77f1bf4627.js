4fe9a2ab2d446f562d12dc2fcd042f0e8a4d5e18
dkessler <pairing4good@gmail.com>
2020-05-18 11:50:57 -0500

    Updates

---
+++ precache-manifest.a9fda6a556a70767218bda77f1bf4627.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acf124e012d0222f873319189f0604b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d20c8e5c14bf3104604",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d73c75b9fddba3d75790",
+    "url": "/static/js/2.b03b2dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b03b2dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d20c8e5c14bf3104604",
+    "url": "/static/js/main.b5743f3c.chunk.js"
+  },
+  {
+    "revision": "10812e2704e22d7b58eb",
+    "url": "/static/js/runtime-main.f2f184f5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
